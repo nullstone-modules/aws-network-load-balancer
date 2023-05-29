10bf84a4c3ebe1c26d7264c46dfe28fea4b7463a
Brad Sickles <brad.sickles@gmail.com>
2023-05-29 08:34:27 -0400

    Fixed log bucket policy to allow NLB access logs

--- logs_bucket/policy.tf
+++ logs_bucket/policy.tf
@@ -11,48 +11,62 @@ resource "aws_s3_bucket_policy" "default" {
 }
 
 data "aws_elb_service_account" "this" {}
+data "aws_region" "current" {}
+data "aws_caller_identity" "current" {}
 
-data "aws_iam_policy_document" "this" {
-  statement {
-    sid = ""
-
-    principals {
-      type        = "AWS"
-      identifiers = [join("", data.aws_elb_service_account.this.*.arn)]
-    }
-
-    effect = "Allow"
-
-    actions = [
-      "s3:PutObject",
-    ]
-
-    resources = [
-      "arn:aws:s3:::${var.name}/*",
-    ]
-  }
+locals {
+  account_id = data.aws_caller_identity.current.account_id
+  region     = data.aws_region.current.name
 }
 
-data "aws_elb_service_account" "default" {}
-
-data "aws_iam_policy_document" "default" {
+// See https://docs.aws.amazon.com/elasticloadbalancing/latest/network/load-balancer-access-logs.html#enable-disable-access-logging
+data "aws_iam_policy_document" "this" {
   statement {
-    sid = ""
+    sid       = "AWSLogDeliveryAclCheck"
+    effect    = "Allow"
+    actions   = ["s3:GetBucketAcl"]
+    resources = ["arn:aws:s3:::${var.name}"]
 
     principals {
-      type        = "AWS"
-      identifiers = [join("", data.aws_elb_service_account.default.*.arn)]
+      type        = "Service"
+      identifiers = ["delivery.logs.amazonaws.com"]
     }
 
-    effect = "Allow"
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceAccount"
+      values   = [local.account_id]
+    }
 
-    actions = [
-      "s3:PutObject",
-    ]
+    condition {
+      test     = "ArnLike"
+      variable = "aws:SourceArn"
+      values   = ["arn:aws:logs:${local.region}:${local.account_id}:*"]
+    }
+  }
 
-    resources = [
-      "arn:aws:s3:::${var.name}/*",
-    ]
+  statement {
+    sid       = "AWSLogDeliveryWrite"
+    effect    = "Allow"
+    actions   = ["s3:PutObject"]
+    resources = ["arn:aws:s3:::${var.name}/AWSLogs/${local.account_id}/*"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["delivery.logs.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceAccount"
+      values   = [local.account_id]
+    }
+
+    condition {
+      test     = "ArnLike"
+      variable = "aws:SourceArn"
+      values   = ["arn:aws:logs:${local.region}:${local.account_id}:*"]
+    }
   }
 }
 

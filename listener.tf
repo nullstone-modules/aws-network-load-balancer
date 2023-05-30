4cff8057b50f38b327410a61986a627a0bd29df6
Brad Sickles <brad.sickles@gmail.com>
2023-05-30 16:44:15 -0400

    Combine target group, listener, and security group rules to prevent collisions

--- listener.tf
+++ listener.tf
@@ -3,28 +3,7 @@
 // See https://docs.aws.amazon.com/elasticloadbalancing/latest/network/target-group-register-targets.html#target-security-groups
 
 resource "aws_security_group_rule" "app-from-world" {
-  security_group_id = local.app_security_group_id
-  type              = "ingress"
-  protocol          = "-1"
-  from_port         = local.port
-  to_port           = local.port
-  cidr_blocks       = ["0.0.0.0/0"]
-}
-
-resource "aws_lb_listener" "this" {
-  load_balancer_arn = aws_lb.this.arn
-  protocol          = var.protocol
-  port              = local.port
-  tags              = local.tags
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.this.arn
-  }
-}
-
-resource "aws_security_group_rule" "additional-from-world" {
-  for_each = local.addl_ports
+  for_each = local.all_ports
 
   security_group_id = local.app_security_group_id
   type              = "ingress"
@@ -34,8 +13,8 @@ resource "aws_security_group_rule" "additional-from-world" {
   cidr_blocks       = ["0.0.0.0/0"]
 }
 
-resource "aws_lb_listener" "additional" {
-  for_each = local.addl_ports
+resource "aws_lb_listener" "this" {
+  for_each = local.all_ports
 
   load_balancer_arn = aws_lb.this.arn
   protocol          = var.protocol
--- target-group.tf
+++ target-group.tf
@@ -1,28 +1,5 @@
 resource "aws_lb_target_group" "this" {
-  name                 = "${local.resource_name}-${local.port}"
-  port                 = local.port
-  protocol             = var.protocol
-  target_type          = "ip"
-  vpc_id               = local.vpc_id
-  deregistration_delay = 10
-  tags                 = local.tags
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  health_check {
-    interval            = var.health_check_interval
-    healthy_threshold   = var.health_check_healthy_threshold
-    unhealthy_threshold = var.health_check_unhealthy_threshold
-    enabled             = var.health_check_enabled
-    timeout             = var.health_check_timeout
-    protocol            = var.protocol
-  }
-}
-
-resource "aws_lb_target_group" "additional" {
-  for_each = local.addl_ports
+  for_each = local.all_ports
 
   name                 = "${local.resource_name}-${each.value}"
   port                 = each.value

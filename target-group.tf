ea88a41f1096ee69d1c078ab381c7d85a82f572d
Brad Sickles <brad.sickles@gmail.com>
2023-05-30 14:26:39 -0400

    Added var.additional_ports to forward additional ports to app (#1)

--- target-group.tf
+++ target-group.tf
@@ -20,3 +20,30 @@ resource "aws_lb_target_group" "this" {
     protocol            = var.protocol
   }
 }
+
+resource "aws_lb_target_group" "additional" {
+  for_each = var.additional_ports
+
+  name                 = "${local.resource_name}-${each.value}"
+  port                 = each.value
+  protocol             = var.protocol
+  target_type          = "ip"
+  vpc_id               = local.vpc_id
+  deregistration_delay = 10
+  tags                 = local.tags
+
+  health_check {
+    // Perform health check on app's primary port
+    port                = local.port
+    interval            = var.health_check_interval
+    healthy_threshold   = var.health_check_healthy_threshold
+    unhealthy_threshold = var.health_check_unhealthy_threshold
+    enabled             = var.health_check_enabled
+    timeout             = var.health_check_timeout
+    protocol            = var.protocol
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
